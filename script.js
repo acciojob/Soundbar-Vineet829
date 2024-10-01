cdc39d50bf74c381c101184c5a58173dd344db01
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-10-01 14:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-document.querySelector("#buttons").addEventListener(("click"), (e) => {
-  if(e.target.tagName === "BUTTON"){
-	  let sound = e.target.textContent
-	 let  audio = new Audio(`sounds/${sound}.mp3`)
-	  audio.play()
-  }
-		  
-  
+document.querySelector("#buttons").addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+        let sound = e.target.textContent;
+        let audio = new Audio(`sounds/${sound}.mp3`);
+        audio.play();
+    }
+});
 
-
-		  
-  }
-	
-})
