1926cb2ec36ff83896928c5eb12760a6428c108f
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-10-01 15:01:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.querySelector("#buttons").addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
         let sound = e.target.textContent;
-        let audio = new Audio(`sounds/${sound}.mp3`);
-        audio.play();
+        let audio = new Audio(`./sounds/${sound}.ogg`);
+
+        audio.addEventListener('error', () => {
+            console.error(`Error loading audio: sounds/${sound}.mp3`);
+        });
+
+        audio.play().catch((error) => {
+            console.error('Error playing audio:', error);
+        });
     }
 });
-
