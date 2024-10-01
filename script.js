93215694a29ecc9ae8c77b466f159e94e2714974
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-10-01 15:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-document.querySelector("#buttons").addEventListener("click", (e) => {
-    if (e.target.tagName === "BUTTON") {
-        let sound = e.target.textContent;
-        let audio = new Audio(`./sounds/${sound}.ogg`);
-
-        audio.addEventListener('error', () => {
-            console.error(`Error loading audio: sounds/${sound}.mp3`);
-        });
-
-        audio.play().catch((error) => {
-            console.error('Error playing audio:', error);
-        });
-    }
-});
+function playsound(soundfile){
+	const audio_ele=document.getElementById("audio");
+	while(audio_ele.firstChild){
+		audio_ele.removeChild(audio_ele.firstChild);
+	}
+	const audio_source=document.createElement('source');
+	audio_source.src=`sound/${soundfile}`;
+	audio_ele.appendChild(audio_source);
+	audio_ele.load();
+	audio_ele.play();
+	return true;
+}
+function stopsound() {
+	const audio_ele=document.getElementById("audio");
+	audio_ele.pause();
+	audio_ele.currentTime=0;
+}
