d4f973873f56bf029b8d39ee8c5237c5a1188d88
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-10-01 14:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector("#buttons").addEventListener(("click"), (e) => {
-  if(e.target.tagName = "BUTTON"){
+  if(e.target.tagName === "BUTTON"){
 	  let sound = e.target.textContent
 	 let  audio = new Audio(`sounds/${sound}.mp3`)
 	  audio.play()
